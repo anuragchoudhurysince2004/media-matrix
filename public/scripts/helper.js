768b05d4951863ba4607ef0941a1defbc7eac14d
Anurag choudhury <idanurag567@gmail.com>
2023-10-04 23:29:55 +0530

    fixed a very minor bug

--- public/scripts/helper.js
+++ public/scripts/helper.js
@@ -1,2 +1,2 @@
-export const base_url = "https://www.media-matrix.up.railway.app";
+export const base_url = "https://media-matrix.up.railway.app";
 // export const base_url = "http://127.0.0.1:3000";
