2d52bb5fdb680ec5fc73c8915e0d4eaf575be81b
Anurag choudhury <idanurag567@gmail.com>
2023-10-04 23:17:58 +0530

    fixed small bug

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -28,7 +28,7 @@ const logout = async () => {
   try {
     const res = await axios({
       method: "GET",
-      url: "http://127.0.0.1:3000/api/v1/auth/logout",
+      url: `${base_url}/api/v1/auth/logout`,
     });
     if ((res.data.status = "success"))
       showAlert("success", "Log out successful");
